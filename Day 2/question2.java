914c0311df95bf3514ec61ce032c23e209895066
kart1k3y <kartikpatil039@gmail.com>
2024-06-24 02:21:55 +0530

    Question 2 solved

---
+++ Day 2/question2.java
@@ -0,0 +1,20 @@
+// In a program, input the side of a square. You have to output the area of the square.
+
+import java.util.*;
+
+public class question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // input side of square
+        System.out.println("Enter Side of a Square : ");
+
+        int side = sc.nextInt();
+
+        // calculate area
+        int area = side * side;
+
+        // display area
+        System.out.println("Area : " + area);
+    }
+}
