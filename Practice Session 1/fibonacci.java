943e0172e7840e2470a9efdb173ac59fe0b9dd4a
kart1k3y <kartikpatil039@gmail.com>
2024-06-26 22:18:29 +0530

    practice session 1

---
+++ Practice Session 1/fibonacci.java
@@ -0,0 +1,12 @@
+// fibonacci series upto n numbers
+
+import java.util.*;
+
+public class fibonacci {
+    public static void main(String args[]) {
+        int f1 = 0;
+        int f2 = 1;
+        int f3;
+        System.out.print(f1,f2)
+    }
+}
