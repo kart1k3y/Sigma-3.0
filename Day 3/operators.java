64e43385109c553ff737b909c999e758217e66be
kart1k3y <kartikpatil039@gmail.com>
2024-07-01 22:46:19 +0530

    Day 3 done

---
+++ Day 3/operators.java
@@ -0,0 +1,23 @@
+// types of operators 
+
+public class operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        //Arithmetic Operators
+
+        System.out.println("A + B = " + (a+b));
+        System.out.println("A - B = " + (a-b));
+        System.out.println("A * B = " + a*b);
+        System.out.println("A / B = " + a/b);
+        System.out.println("A % B = " + a%b);
+
+        //Increment operators
+
+        int c = 10,d=c++;
+        System.out.println(c+ " ," + d);
+
+        //Other types : Relations , Assignment , Bitwise , Logical
+    }
+}
--- Practice Session 1/fibonacci.java
+++ Practice Session 1/fibonacci.java
@@ -4,9 +4,18 @@ import java.util.*;
 
 public class fibonacci {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
         int f1 = 0;
         int f2 = 1;
         int f3;
-        System.out.print(f1,f2)
+        System.out.print(f1 + "\t" + f2);
+        for(int i = 0 ; i <=n ; i++) {
+            f3= f1+f2;
+            System.out.print("\t" + f3);
+            f1=f2;
+            f2=f3;
+        }
     }
 }
